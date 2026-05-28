d62ae0f76291ad7a29d96c54fcbb08ef7b3d466e
Divakar333B <divakarbabu5533@gmail.com>
2026-05-28 16:17:31 +0530

    fix: change all settings defaults to production module

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,13 +4,14 @@ WSGI config for config project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://djangoproject.com
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# Force Django to look into settings/production.py on your production server
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
