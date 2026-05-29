99b8bd4ba9ad4b2ff5f62c97f4e3f1c9006edbf0
Divakar333B <divakarbabu5533@gmail.com>
2026-05-29 16:44:05 +0530

    changed the billing url

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -8,7 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('<int:order_id>/generate/', GenerateBillView.as_view(),  name='generate-bill'),
+    # Change this line:
+    path('generate/<int:order_id>/', GenerateBillView.as_view(), name='generate-bill'),
+    
     path('<int:order_id>/',          BillDetailView.as_view(),    name='bill-detail'),
     path('<int:bill_id>/paid/',      MarkPaidView.as_view(),      name='mark-paid'),
     path('today/',                   TodayBillsView.as_view(),    name='today-bills'),
